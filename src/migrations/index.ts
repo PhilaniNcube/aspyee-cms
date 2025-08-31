e5b63a85f3e8e5ef5b1ab07db0ca1be1ccb890e1
Philani Ncube <ncbphi001@gmail.com>
2025-08-31 17:09:27 +0200

    Add migration for countries collection and update migration index
    - Created a new migration file for the countries collection (20250831_150813_countries_collection).
    - Added the corresponding JSON schema for the countries collection.
    - Updated the migration index to include the new migration.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250831_120127 from './20250831_120127';
 import * as migration_20250831_145523 from './20250831_145523';
+import * as migration_20250831_150813_countries_collection from './20250831_150813_countries_collection';
 import * as migration_20250831_countries_seed from './20250831_countries_seed';
 
 export const migrations = [
@@ -12,6 +13,11 @@ export const migrations = [
     up: migration_20250831_145523.up,
     down: migration_20250831_145523.down,
     name: '20250831_145523',
+  },
+  {
+    up: migration_20250831_150813_countries_collection.up,
+    down: migration_20250831_150813_countries_collection.down,
+    name: '20250831_150813_countries_collection',
   },
   {
     up: migration_20250831_countries_seed.up,
