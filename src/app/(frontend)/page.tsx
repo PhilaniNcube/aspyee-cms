738192ee5bf0ca5d55c0e63bc9d4f485dc6d7fa6
Philani Ncube <ncbphi001@gmail.com>
2025-09-01 17:40:27 +0200

    feat: redirect HomePage to Knowledge Centre

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,6 +1,9 @@
+import { redirect } from 'next/navigation'
 import Hero from './knowledge-centre/_components/hero'
 
 export default async function HomePage() {
+  redirect('/knowledge-centre')
+
   return (
     <div>
       <Hero />
