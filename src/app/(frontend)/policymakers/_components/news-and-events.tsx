9bab95fdc5e5ab6708831ad9cf74c505dfae7ad8
Philani Ncube <ncbphi001@gmail.com>
2025-09-07 11:55:28 +0200

    feat: add relative positioning to news and events container for improved layout

--- src/app/(frontend)/policymakers/_components/news-and-events.tsx
+++ src/app/(frontend)/policymakers/_components/news-and-events.tsx
@@ -50,7 +50,7 @@ export default function NewsEvents() {
 
   return (
     <div className=" bg-gray-50 py-12">
-      <div className="max-w-[1440px] px-6 md:px-10 lg:px-16 container mx-auto flex items-center justify-center">
+      <div className="max-w-[1440px] px-6 md:px-10 lg:px-16 container mx-auto flex items-center justify-center relative">
         <Image
           src="https://4kav3digtb.ufs.sh/f/FBDFb9YX4geOoDag490r6Jgl0qAyzZvwDEbQ3WopUuCPcOkY"
           alt="Logo"
@@ -58,6 +58,11 @@ export default function NewsEvents() {
           height={933}
           className="mb-8 w-full  object-cover rounded-lg shadow-lg max-h-[60vh] object-left-center"
         />
+        <div className="absolute inset-0 flex justify-start items-end px-10 py-7 bg-from-black/0 via-black/20 to-black/60 bg-gradient-to-t rounded-lg">
+          <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white drop-shadow-lg">
+            EVENTS FOR YOU
+          </h2>
+        </div>
       </div>
     </div>
   )
