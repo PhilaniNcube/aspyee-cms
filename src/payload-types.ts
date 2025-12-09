819ca95be80eed0a0ccf6bd5ebe3734b27549635
PhilaniNcube <ncbphi001@gmail.com>
2025-12-09 17:13:03 +0200

    feat: add PayloadKv and PayloadKvSelect interfaces to Config
    - Introduced PayloadKv interface to define key-value data structure.
    - Added PayloadKvSelect interface for optional selection of key and data.
    - Updated Config interface to include 'payload-kv' and its select variant.
    - Enhanced fallbackLocale type definition for better clarity.

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -77,6 +77,7 @@ export interface Config {
     blogs: Blog;
     events: Event;
     'news-and-events-page': NewsAndEventsPage;
+    'payload-kv': PayloadKv;
     'payload-locked-documents': PayloadLockedDocument;
     'payload-preferences': PayloadPreference;
     'payload-migrations': PayloadMigration;
@@ -93,6 +94,7 @@ export interface Config {
     blogs: BlogsSelect<false> | BlogsSelect<true>;
     events: EventsSelect<false> | EventsSelect<true>;
     'news-and-events-page': NewsAndEventsPageSelect<false> | NewsAndEventsPageSelect<true>;
+    'payload-kv': PayloadKvSelect<false> | PayloadKvSelect<true>;
     'payload-locked-documents': PayloadLockedDocumentsSelect<false> | PayloadLockedDocumentsSelect<true>;
     'payload-preferences': PayloadPreferencesSelect<false> | PayloadPreferencesSelect<true>;
     'payload-migrations': PayloadMigrationsSelect<false> | PayloadMigrationsSelect<true>;
@@ -100,6 +102,7 @@ export interface Config {
   db: {
     defaultIDType: number;
   };
+  fallbackLocale: ('false' | 'none' | 'null') | false | null | ('en' | 'fr') | ('en' | 'fr')[];
   globals: {};
   globalsSelect: {};
   locale: 'en' | 'fr';
@@ -843,6 +846,23 @@ export interface NewsAndEventsPage {
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "payload-kv".
+ */
+export interface PayloadKv {
+  id: number;
+  key: string;
+  data:
+    | {
+        [k: string]: unknown;
+      }
+    | unknown[]
+    | string
+    | number
+    | boolean
+    | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
  * via the `definition` "payload-locked-documents".
  */
 export interface PayloadLockedDocument {
@@ -1163,6 +1183,14 @@ export interface NewsAndEventsPageSelect<T extends boolean = true> {
       };
   updatedAt?: T;
   createdAt?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "payload-kv_select".
+ */
+export interface PayloadKvSelect<T extends boolean = true> {
+  key?: T;
+  data?: T;
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
