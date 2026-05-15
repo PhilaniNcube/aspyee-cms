010bcfad43ae528a5618a646250b220529b9b0d0
PhilaniNcube <ncbphi001@gmail.com>
2026-05-15 10:42:09 +0200

    feat: add centre.aspyee.org to allowed image hostnames in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -36,6 +36,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'centre.aspyee.org',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   // Exclude problematic node_modules from Turbopack processing
